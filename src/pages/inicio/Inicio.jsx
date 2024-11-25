831210f1416b7e861dfeecfaba6c67f9300ad601
RafaGastão <dev.rafaelgastao@gmail.com>
2024-11-25 19:42:05 -0300

    adicionei pagina inicio

--- src/pages/inicio/Inicio.jsx
+++ src/pages/inicio/Inicio.jsx
@@ -1,7 +1,41 @@
+import s from './inicio.module.scss'
+import oferece from '../../assets/Imagem1.png'
+import garante from '../../assets/Imagem2.png'
+import estimula from '../../assets/Imagem3.png'
+import fornece from '../../assets/Imagem4.png'
+
 export default function Inicio(){
     return(
-        <section>
-            <h2>Eu sou componente inicio</h2>
+    <main>
+        <section className={s.primeiraSessaoInicio}>
+            <h2>VENHA FAZER PARTE DA MAIOR REDE DE DOAÇÃO</h2>
         </section>
+
+        <section className={s.porqueDoarsection}>
+            <h3>Por que devo doar?</h3>
+        
+        <div className={s.containerCards}>
+        <section>
+        <img src={oferece} alt="" />
+        <p> Oferece livros a quem não tem acesso, ajudando a reduzir a exclusão social.</p>
+        </section>
+
+         <section>
+        <img src={estimula} alt="" />
+        <p> Estimula o hábito da leitura e o aprendizado contínuo.</p>
+        </section>
+
+         <section>
+        <img src={fornece} alt="" />
+        <p> Fornece conhecimento e inspiração, permitindo que indivíduos transformem suas vidas.</p>
+        </section>
+
+         <section>
+        <img src={garante} alt="" />
+        <p> Garante que todos, independentemente de sua condição, tenham oportunidades de aprendizado.</p>
+        </section>
+    </div>
+    </section>
+    </main>
     )
 }
