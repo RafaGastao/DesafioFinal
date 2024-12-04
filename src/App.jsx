c165410e1990afcfcd0fdeb71bc8d9d6a85b75e9
RafaGastão <dev.rafaelgastao@gmail.com>
2024-12-04 18:40:38 -0300

    renoamento das pastas

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
-import Header from "./components/header/Header"
+import Header from "./components/Header/Header"
 import './globalStyle/globalStyle.scss'
-import Footer from './components/footer/Footer'
+import Footer from './components/Footer/Footer'
 
 
 
